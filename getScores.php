faa64a7d3653b6ddae258d743e63712a76061ef3
jasontowens <jasontowens94@gmail.com>
2014-03-14 03:19:06 -0400

    Changed back button functionality for highScores

--- getScores.php
+++ getScores.php
@@ -1,7 +1,10 @@
 <?php
-	require 'connect.php';
+	require '../connect.php';
 	$count = (int) $_POST['count'];
-	$sql = mysqli_query($con, "SELECT * FROM Snake_Scores ORDER BY Score DESC LIMIT $count") or die(mysql_error());
+	if (!$count) {
+		$count = 5;
+	}
+	$sql = mysqli_query($con, "SELECT * FROM Snake_Scores ORDER BY Score DESC LIMIT $count") or die(mysqli_error($con));
 	$arr = array();
 	while($row = $sql->fetch_assoc()) {
 		$arr[] = $row;
